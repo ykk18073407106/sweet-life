e45070ee01e51cc4fa95e3d83d88e1a29f6cd726
ykk18073407106 <1419065994@qq.com>
2019-09-27 16:08:47 +0800

    修改service

--- src/main/java/com/wnwy/project/SweetLifePavilionApplication.java
+++ src/main/java/com/wnwy/project/SweetLifePavilionApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SweetLifePavilionApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SweetLifePavilionApplication.class, args);
+        SpringApplication.run(SweetLifePavilionApplication.class,args);
     }
 
 }
